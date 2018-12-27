726c3f8c5994138808e08f84ab962d1fbd6ab6f1
Karthikeyan <30459376+Karthikeyankios@users.noreply.github.com>
2018-12-27 10:06:27 +0530

    Initial Commit

---
+++ LeanGithubCommits/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeanGithubCommits
+//
+//  Created by Karthik Baskaran on 27/12/18.
+//  Copyright © 2018 Karthik Baskaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeanGithubCommits/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeanGithubCommits
+//
+//  Created by Karthik Baskaran on 27/12/18.
+//  Copyright © 2018 Karthik Baskaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
